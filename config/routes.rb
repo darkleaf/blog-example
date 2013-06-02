2b856a5ff47b2474f21a86a3cc4b21f3011138f9
Михаил Кузьмин <m.kuzmin@darkleaf.ru>
2013-06-02 10:40:23 +0400

    add welcome

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Blog::Application.routes.draw do
+  scope module: :web do
+    root 'welcome#show'
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
